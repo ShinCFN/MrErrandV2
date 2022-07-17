b49210d56a5b67f6cf35db7b9eedd0a3b973de47
ShinCFN <notheixin@gmail.com>
2022-07-17 12:03:46 +0800

    UI Changes

--- app/src/main/java/com/example/mrerrandv2/SettingsFragment.java
+++ app/src/main/java/com/example/mrerrandv2/SettingsFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,12 +25,14 @@ public class SettingsFragment extends Fragment {
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
 
+    LinearLayout btnLogout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
-        Button btnLogout = v.findViewById(R.id.LogoutNowButton);
+        btnLogout = v.findViewById(R.id.LogoutNowButton);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
