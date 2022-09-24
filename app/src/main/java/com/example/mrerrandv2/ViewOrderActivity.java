c37487add492509b3c43c6ef0f60105076280156
ShinCFN <notheixin@gmail.com>
2022-09-25 02:13:11 +0800

    Fixed rider cancel order on view order

--- app/src/main/java/com/example/mrerrandv2/ViewOrderActivity.java
+++ app/src/main/java/com/example/mrerrandv2/ViewOrderActivity.java
@@ -2,6 +2,7 @@ package com.example.mrerrandv2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -127,8 +128,7 @@ public class ViewOrderActivity extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                DatabaseReference ordRef = FirebaseDatabase.getInstance().getReference("Order").child(ord_open.getUID());
+                DatabaseReference ordRef = FirebaseDatabase.getInstance().getReference("Order").child(ord_open.getKey());
                 ordRef.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
